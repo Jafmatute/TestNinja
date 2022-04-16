b26204f53c258508ced3d530d9f24b2f40b122a6
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-04-16 01:04:21 -0600

    Testing Arrays and Collections

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -34,5 +34,28 @@ namespace TestNinja.UnitTests
             var result = _math.Max(a, b);
             Assert.That(result, Is.EqualTo(expectedResult));
         }
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit()
+        {
+            var result = _math.GetOddNumbers(5);
+
+            //general
+            //Assert.That(result, Is.Not.Empty);
+
+            //Assert.That(result.Count(), Is.EqualTo(3));
+
+            // Assert.That(result,Does.Contain(2));
+            // Assert.That(result,Does.Contain(3));
+            // Assert.That(result,Does.Contain(5));
+
+            Assert.That(result, Is.EquivalentTo(new[] {1, 3, 5}));
+
+            //order
+            //Assert.That(result,Is.Ordered);
+
+            //elements unique array
+            //Assert.That(result, Is.Unique);
+        }
     }
 }
