8cefdf27880fe52a275d59be77986624ebe87946
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-04-15 21:48:47 -0600

    Set Up and Tear Down

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -6,12 +6,20 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathTests
     {
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+        //SetUp
+        //TearDown
+
+        private Math _math;
+
         [Test]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
-            var math = new Math();
-
-            var result = math.Add(1, 2);
+            var result = _math.Add(1, 2);
 
             Assert.That(result, Is.EqualTo(3));
         }
@@ -19,24 +27,21 @@ namespace TestNinja.UnitTests
         [Test]
         public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
         {
-            var math = new Math();
-            var result = math.Max(2, 1);
+            var result = _math.Max(2, 1);
             Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
         {
-            var math = new Math();
-            var result = math.Max(1, 2);
+            var result = _math.Max(1, 2);
             Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
         {
-            var math = new Math();
-            var result = math.Max(2, 2);
+            var result = _math.Max(2, 2);
             Assert.That(result, Is.EqualTo(2));
         }
     }
