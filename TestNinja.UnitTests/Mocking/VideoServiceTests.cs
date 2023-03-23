0090f9366a692169e6d4175b141f779ac2d83e2c
codewithJosue <repositorydevcode@gmail.com>
2023-03-23 13:57:35 -0600

    Dependency Injection via Constructor

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -9,12 +9,10 @@ namespace TestNinja.UnitTests.Mocking
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService
-            {
-                FileReader = new FakeFileReader()
-            };
-
-            var result= service.ReadVideoTitle();
+            var service = new VideoService(new FakeFileReader());
+            //service.FileReader = new FakeFileReader();
+            
+           var result= service.ReadVideoTitle();
             
             Assert.That(result, Does.Contain("error").IgnoreCase);
             
