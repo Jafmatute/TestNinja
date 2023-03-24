f3f40e827093c9f3b0a0f6c79e396d7a13d7243f
codewithJosue <repositorydevcode@gmail.com>
2023-03-24 09:25:11 -0600

    An Example of a Mock Abuse

---
+++ TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount()
+        {
+            var product = new Product {ListPrice = 100};
+
+           var result= product.GetPrice(new Customer {IsGold = true});
+            
+            Assert.That(result, Is.EqualTo(70));
+            
+        }
+        
+        //error al escribir pruebas con simulación no hacer esto!
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount_2MoqErrorUsing()
+        {
+            var customer = new Mock<ICustomer>();
+            customer.Setup(c => c.IsGold).Returns(true);
+            
+            var product = new Product {ListPrice = 100};
+
+            var result= product.GetPrice(customer.Object);
+            
+            Assert.That(result, Is.EqualTo(70));
+            
+            
+        }
+    }
+}
