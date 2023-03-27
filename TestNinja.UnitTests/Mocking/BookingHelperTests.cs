4e655723f32191b7a50e0a2c5bcf71ec380e8c0d
codewithJosue <repositorydevcode@gmail.com>
2023-03-27 13:59:15 -0600

    exercises  BookingHelper tests cases

---
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class BookingHelperTests
+    {
+        [Test]
+        public void OverlappingBookingsExist_BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
+        {
+            
+        }
+    }
+}
